1540e219d40462a88efcf47d92b88c073733418a
tuhucon <minhtuan93@yahoo.com>
2024-07-08 13:58:06 +0700

    tiup playground --db 3 create a TiDB cluser with 3 tidb server then connect to cluster from application hikari will create 4 connections to each server.

---
+++ src/main/java/com/example/tidbspringboot/TidbSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.example.tidbspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TidbSpringbootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TidbSpringbootApplication.class, args);
+    }
+
+}
